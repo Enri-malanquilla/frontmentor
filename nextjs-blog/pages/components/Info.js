d7315ed65eb6c5b530767694b3644836f4c8e969
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-12-21 18:08:42 +0100

    terminado proyectillo

--- nextjs-blog/pages/components/Info.js
+++ nextjs-blog/pages/components/Info.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
 export const Info = ({ id, act, time, daily, weekly, monthly }) => {
+  const [open, setOpen] = useState(true);
+
   return (
     <section className='info' id={id.toLowerCase().replace(' ', '-')}>
       <aside>
@@ -8,13 +10,19 @@ export const Info = ({ id, act, time, daily, weekly, monthly }) => {
       </aside>
       <article>
         <h1>{id}</h1>
-        <button className='abrir-cerrar'>...</button>
-        {(daily && <h2>{`${time.daily.current}hrs`}</h2>) ||
-          (weekly && <h2>{`${time.weekly.current}hrs`}</h2>) ||
-          (monthly && <h2>{`${time.monthly.current}hrs`}</h2>)}
-        {(daily && <p>Previous - {`${time.daily.previous}`}hrs</p>) ||
-          (weekly && <p>Previous - {`${time.weekly.previous}`}hrs</p>) ||
-          (monthly && <p>Previous - {`${time.monthly.previous}`}hrs</p>)}
+        <button className='abrir-cerrar' onClick={(e) => setOpen(!open)}>
+          ...
+        </button>
+        {open && (
+          <>
+            {(daily && <h2>{`${time.daily.current}hrs`}</h2>) ||
+              (weekly && <h2>{`${time.weekly.current}hrs`}</h2>) ||
+              (monthly && <h2>{`${time.monthly.current}hrs`}</h2>)}
+            {(daily && <p>Previous - {`${time.daily.previous}`}hrs</p>) ||
+              (weekly && <p>Previous - {`${time.weekly.previous}`}hrs</p>) ||
+              (monthly && <p>Previous - {`${time.monthly.previous}`}hrs</p>)}
+          </>
+        )}
       </article>
     </section>
   );
